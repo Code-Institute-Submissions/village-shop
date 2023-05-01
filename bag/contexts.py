0b0a9ecaec6fb7794ef1389cec1d86fe0dc164d6
vicgoodall <vic.goodall@icloud.com>
2023-05-01 12:47:14 +0000

    added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,9 +9,9 @@ def bag_contents(request):
     product_count = 0
 
     context = {
-        bag_items: bag_items,
-        total: total,
-        product_count: product_count,
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
     }
 
     return context
