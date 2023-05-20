e729632763029532bdfc19bde6d05736fac67e9d
vicgoodall <vic.goodall@icloud.com>
2023-05-20 13:36:55 +0000

    Added ability to edit products

--- products/views.py
+++ products/views.py
@@ -72,6 +72,7 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 def add_product(request):
     """ Add product to the store """
     if request.method == 'POST':
@@ -84,10 +85,33 @@ def add_product(request):
             messages.error(request, 'Failed to add product. Please check the entry details are correct.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
 
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product was updated.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update. Please check all details are correct and retry')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
     return render(request, template, context)
