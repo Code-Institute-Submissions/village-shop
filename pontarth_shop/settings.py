6f14a8c5235b3d5d7eb610d1c3e0ce68ee9011e6
vicgoodall <vic.goodall@icloud.com>
2023-05-23 20:41:35 +0000

    Removed secret key and updated debug

--- pontarth_shop/settings.py
+++ pontarth_shop/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mxpp^t)9h3hie%f+(yn*2vk%)a)6gjntf!&xg4$0+srrjc1l2k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pontarth-shop.herokuapp.com', 'localhost']
 
